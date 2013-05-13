5ffeb293857218071e86f03ee53910f24b02b6fc
elyl <elylll@gmail.com>
2013-05-13 13:45:16 +0200

    Fin traitements de la liste

--- compression/clempel.c
+++ compression/clempel.c
@@ -4,6 +4,7 @@
 void clempel(char *str)
 {
   t_list	*st;
+  t_list	*tmp;
   
   st = NULL;
   st = add_to_list("toto", st);
@@ -11,5 +12,7 @@ void clempel(char *str)
   add_to_list("tutu", st);
   add_to_list("tata", st);
   print_list(st);
+  tmp = get_from_list("tutu", st);
+  printf("\n<%d>%s\n", tmp->code, tmp->str);
   free_list(st);
 }
