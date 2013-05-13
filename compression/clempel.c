8cec4e25c7b6fa731f71c9f1f92b96d48c141b1e
elyl <elylll@gmail.com>
2013-05-13 21:00:43 +0200

    Optimisation

--- compression/clempel.c
+++ compression/clempel.c
@@ -8,13 +8,11 @@ void clempel(char *str)
   t_list	*tmp;
   char		w[255];
   char		wc[255];
-  char		c;
   
   st = init_list();
   w[0] = '\0';
   while (*str)
     {
-      strncpy(&c, str, 1);
       strcpy(&wc[0], &w[0]);
       strncat(&wc[0], str, 1);
       tmp = get_from_list(&wc[0], st);
@@ -23,7 +21,7 @@ void clempel(char *str)
       else
 	{
 	  add_to_list(&wc[0], st);
-	  strncpy(&w[0], str, 1);
+	  w[0] = *str;
 	  w[1] = '\0';
 	}
       str++;
