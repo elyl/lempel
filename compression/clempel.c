4dcde25f9b963f05efd72d36d6e87e6e24ec3379
elyl <elylll@gmail.com>
2013-05-30 09:50:07 +0200

    Amélioration algo

--- compression/clempel.c
+++ compression/clempel.c
@@ -5,7 +5,31 @@
 void clempel(char *str)
 {
   t_list	*st;
+  t_list	*tmp;
+  char		w[5];
+  char		wc[255];
+  char		c;
   
   st = init_list();
+  w[0] = '\0';
+  while (*str)
+    {
+      strncpy(&c, str, 1);
+      strcpy(&wc[0], &w[0]);
+      strncat(&wc[0], str, 1);
+      tmp = get_from_list(&wc[0], st);
+      printf("w:%s c:%c wc:%s\n", &w[0], c, &wc[0]);
+      if (tmp != NULL)
+	strcpy(&w[0], &wc[0]);
+      else
+	{
+	  add_to_list(&wc[0], st);
+	  strncpy(&w[0], str, 1);
+	  w[1] = '\0';
+	}
+      //printf("w:%s c:%c wc:%s\n", &w[0], c, &wc[0]);
+      str++;
+    }
+  print_list(st);
   free_list(st);
 }
