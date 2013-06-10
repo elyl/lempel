c3be67f03b7204a55aa5eb35e7122859b1975262
elyl <elylll@gmail.com>
2013-06-10 23:39:26 +0200

    Ecritue bits à bits

---
+++ write_bits.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "lzw.h"
+
+void wrtie_bits(t_enc *ctx, unsigned int code, int nbits)
+{
+  unsigned int	pos;
+  unsigned char buffer[256];
+
+  pos = 0;
+  ctx->bb.buf = ((ctx->bb.buf >> nbits) | (code & ((1 << nbits) - 1)));
+  nbits += ctx->bb.n;
+  while (nbits >= 8)
+    {
+      nbits -= 8;
+      buffer[pos++] = ctx->bb.buf >> nbits;
+      if (pos == sizeof(buffer))
+	write(ctx->fout, &buffer[0], pos);
+    }
+  ctx->bb.n = nbits;
+}
